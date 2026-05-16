a55a0e34942a1cbde6ac358ca91bb7ec06fca64f
Juana-archer <faniry1d@gmail.com>
2026-05-16 11:37:54 +0300

    Restructure: déplacement de tout dans le dossier insta_kendou

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="insta_kendou",
     version="1.0.0",
-    description="bibliotheque",
+    description="Bibliothèque Instagram",
     author="Juana-archer",
     packages=find_packages(),
-    install_requires=[
-        # Mettez vos dépendances ici si nécessaire
-        # Exemple: "requests>=2.25.0",
-    ],
     python_requires=">=3.7",
 )
